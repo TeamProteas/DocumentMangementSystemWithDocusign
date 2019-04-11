d2cc35c53d605ee32d6e17ac9ddaecba95ae6c9b
tanejaanmol <anmol.taneja@ninelaps.com>
2019-04-11 17:28:40 +0530

    email notification system added

---
+++ DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/controller/EmailNotificationController.java
@@ -0,0 +1,45 @@
+package com.nineleaps.DocumentManagementSystem.controller;
+
+import com.nineleaps.DocumentManagementSystem.dto.EmailNotificationData;
+import com.nineleaps.DocumentManagementSystem.service.Impl.EmailNotificationImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+
+@CrossOrigin
+@RestController
+public class EmailNotificationController {
+
+    @Autowired
+    EmailNotificationImpl notificationImpl;
+
+    @RequestMapping("/v1/sendmail")
+    public String signUpSuccess(@RequestHeader("tokenId") String tokenId, @RequestBody EmailNotificationData emailNotificationData) {
+
+        //send a notification
+        try {
+            notificationImpl.sendNotification(emailNotificationData);
+            return "Successfully signed in";
+        } catch (MailException e) {
+            System.out.println(e.getMessage());
+            return "mail not sent";
+        }
+    }
+
+    @RequestMapping("/v1/sendhtml")
+    public String htmlMail(@RequestHeader("tokenId") String tokenId, @RequestBody EmailNotificationData emailNotificationData) {
+        Context context = new Context();
+        context.setVariable("title", "REMINDER MAIL");
+        context.setVariable("name", emailNotificationData.getName());
+        context.setVariable("description", emailNotificationData.getDescription());
+        try {
+            notificationImpl.sendHtmlMail(emailNotificationData.getEmailAddress(), "template", context);
+        } catch (MessagingException e) {
+            System.out.println(e.getMessage());
+        }
+        return "html mail sent";
+    }
+}
