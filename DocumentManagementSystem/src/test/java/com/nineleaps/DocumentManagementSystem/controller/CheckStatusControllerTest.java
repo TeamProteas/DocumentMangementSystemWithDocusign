2511cfec396f777e58d0e2b64faa4e47c8fc644a
Siddhant <siddhant.sharma@nineleaps.com>
2019-04-15 16:35:15 +0530

    changes in controller testing

--- DocumentManagementSystem/src/test/java/com/nineleaps/DocumentManagementSystem/controller/CheckStatusControllerTest.java
+++ DocumentManagementSystem/src/test/java/com/nineleaps/DocumentManagementSystem/controller/CheckStatusControllerTest.java
@@ -13,13 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
+import static sun.rmi.transport.TransportConstants.Return;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
@@ -31,6 +34,9 @@ public class CheckStatusControllerTest {
     CheckStatusImpl checkStatusImpl;
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
     @Before
     public void setup() {
 
@@ -47,16 +53,28 @@ public class CheckStatusControllerTest {
         when(checkStatusImpl.checkStatus("mukul","pancard")).thenReturn("Signed");
 
 
-        mockMvc.perform(MockMvcRequestBuilders.post("v1/checkstatus")
+      MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/v1/checkstatus")
                 .accept(MediaType.ALL)
-                .contentType(MediaType.ALL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+
                 .header("tokenId", "abcde")
                 .param("name", "mukul")
-                .param("documentname", "pancard"));
+                .param("documentname", "pancard")
 
 
-        String test=checkStatusController.status("abcde","mukul","pancard");
-        assertEquals(test,"Signed");
+      ).andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+       int status = mvcResult.getResponse().getStatus();
+       assertEquals(200,status);
+
+
+
+
+       assertEquals(content,"Signed");
+
+
+        //String test=checkStatusController.status("abcde","mukul","pancard");
+        //assertEquals(test,"Signed");
 
 
 
---
+++ DocumentManagementSystem/src/test/java/com/nineleaps/DocumentManagementSystem/exceptions/SignInInvalidTokenErrorTest.java
@@ -0,0 +1,15 @@
+package com.nineleaps.DocumentManagementSystem.exceptions;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class SignInInvalidTokenErrorTest {
+
+
+
+}
