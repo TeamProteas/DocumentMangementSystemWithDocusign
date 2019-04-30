04faae5aab6c41d138929c1ac6daa8cb2b96ea38
tanejaanmol <anmol.taneja@ninelaps.com>
2019-04-30 19:26:31 +0530

    get file controller changed

--- DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/controller/GetSignFileController.java
+++ DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/controller/GetSignFileController.java
@@ -2,10 +2,12 @@ package com.nineleaps.DocumentManagementSystem.controller;
 
 import com.nineleaps.DocumentManagementSystem.exceptions.CustomResponse;
 import com.nineleaps.DocumentManagementSystem.service.Impl.GetSignFileImpl;
-import okio.ByteString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
@@ -16,8 +18,9 @@ public class GetSignFileController {
     @Autowired
     GetSignFileImpl getSignFileImpl;
 
-    @GetMapping("/v1/getfile")
+    @RequestMapping("/v1/getfile")
     public ResponseEntity<CustomResponse> getFile(@RequestParam("name") String name, @RequestParam("documentname") String documentname) throws IOException {
+        System.out.println("GETFILE");
         return getSignFileImpl.getFile(name, documentname);
 
     }
